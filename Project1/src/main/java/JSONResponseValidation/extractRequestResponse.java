8de2a81a8ba730656b1742b463a90fe9742416f2
ravit <ravit@CRTV>
2023-04-19 12:20:07 +0530

    1 commit for Rest Assured Automation April 19

---
+++ Project1/src/main/java/JSONResponseValidation/extractRequestResponse.java
@@ -0,0 +1,38 @@
+package JSONResponseValidation;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class extractRequestResponse {
+	
+	@Test
+	public void extractResponse() {
+		RestAssured.baseURI = "https://api.github.com";
+		String bearerToken = "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";
+		
+
+		Response response = given()
+			   .header("Content-Type", "application/json")
+			   .header("Authorization", "Bearer " + bearerToken)
+			   .
+		when()
+				.get("/user/repos").
+		then().assertThat().statusCode(200).and().
+			   contentType(ContentType.JSON).
+	    extract().response();
+		
+		String jsonResponse = response.asString();
+		
+		JsonPath responseBody = new JsonPath(jsonResponse);
+		System.out.println("0th Repo Name : " + responseBody.get("[0].name"));
+		System.out.println("1th Repo Name : " + responseBody.get("[1].name"));
+				
+	}
+
+}
